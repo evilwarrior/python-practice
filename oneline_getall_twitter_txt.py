23ff2b597d91cc03d41bdd93a76dfd38e8fce5d3
evilwarrior <46455753+evilwarrior@users.noreply.github.com>
2024-03-10 00:50:07 +0000

    Create oneline_getall_twitter_txt.py

---
+++ oneline_getall_twitter_txt.py
@@ -0,0 +1,3 @@
+import os, re
+
+print('\n'.join(filter(lambda txt: 'axfc' in txt, map(lambda file: '\n'.join((re.match(r'^SHIELD1001-(\d+)-.*\.txt$', file).group(1), open(file, 'r', encoding='utf8').read())), filter(lambda file: file != 'a.py', os.listdir())))))
