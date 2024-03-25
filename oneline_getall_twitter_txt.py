79fa61882dbd74bbff872f442f9ba3afbf502fc4
evilwarrior <46455753+evilwarrior@users.noreply.github.com>
2024-03-25 14:29:56 +0000

    Update oneline_getall_twitter_txt.py

--- oneline_getall_twitter_txt.py
+++ oneline_getall_twitter_txt.py
@@ -1,3 +1,4 @@
 import os, re
 
 print('\n'.join(filter(lambda txt: 'axfc' in txt, map(lambda file: '\n'.join((re.match(r'^SHIELD1001-(\d+)-.*\.txt$', file).group(1), open(file, 'r', encoding='utf8').read())), filter(lambda file: file != 'a.py', os.listdir())))))
+print('\n'.join(filter(lambda txt: 'axfc' in txt, map(lambda file: '\n'.join((re.match(r'^asakota0-(\d+)-.*\.txt$', file).group(1), open(file, 'r', encoding='utf8').read())), filter(lambda file: file != 'a.py', os.listdir())))))
