97b25bf3f19045b640d0fc01e864277361d0b88c
evilwarrior <46455753+evilwarrior@users.noreply.github.com>
2024-05-15 18:03:15 +0000

    Update oneline_getall_twitter_txt.py

--- oneline_getall_twitter_txt.py
+++ oneline_getall_twitter_txt.py
@@ -1,4 +1,3 @@
-import os, re
-
+import os, re, itertools
 print('\n'.join(filter(lambda txt: 'axfc' in txt, map(lambda file: '\n'.join((re.match(r'^SHIELD1001-(\d+)-.*\.txt$', file).group(1), open(file, 'r', encoding='utf8').read())), filter(lambda file: file != 'a.py', os.listdir())))))
-print('\n'.join(filter(lambda txt: 'axfc' in txt, map(lambda file: '\n'.join((re.match(r'^asakota0-(\d+)-.*\.txt$', file).group(1), open(file, 'r', encoding='utf8').read())), filter(lambda file: file != 'a.py', os.listdir())))))
+print('\n'.join(itertools.chain.from_iterable(filter(lambda pair: 'axfc' in pair[1].lower(), map(lambda file: (file.split('-')[1], open(file, 'r', encoding='utf8').read().lower()), filter(lambda file: file != 'a.py', os.listdir()))))))
